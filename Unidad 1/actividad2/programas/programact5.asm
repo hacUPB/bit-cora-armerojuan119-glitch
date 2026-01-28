9de1768c7e91c802e545dec5bead8819e4b42269
armerojuan119-glitch <armerojuan119@gmail.com>
2026-01-28 14:32:10 -0500

    ensAYO y error

--- Unidad 1/actividad2/programas/programact5.asm
+++ Unidad 1/actividad2/programas/programact5.asm
@@ -1,33 +1,21 @@
-// --- CÓDIGO CON ERROR FATAL 3 ---
 @1
 D=A
 @0
-M=D      // RAM[0] = 1 (Contador)
-
-@12
-M=0      // RAM[12] = 0 (Acumulador)
+M=D
 
 (LOOP)
-    @0
-    D=M
     @5
     D=D-A
     @END
-    D;JGT    // Salida del ciclo (esto está bien)
+    D;JGT
 
-    // ERROR FATAL AQUÍ:
+    // ERROR AQUÍ:
     @0
-    D=A      // <--- ¡ERROR! Estoy guardando el número 0 (la dirección), no el valor de i
+    M=M+M    // Intenté sumar el contador a sí mismo directamente
+    
     @12
-    M=D+M    // sum = sum + 0 (No suma nada o suma la dirección)
-
-    @0
-    M=M+1    // i = i + 1
+    M=M+0    // Intenté sumar RAM[0] a RAM[12] sin usar D
     
     @LOOP
-    0;JMP
-
-(END)
-    @END
     0;JMP
     
